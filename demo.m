47b083dd165f943824d49fdb004c9a7b62c07fbc
JunlinXu <47915065+JunlinXu@users.noreply.github.com>
2020-07-31 21:47:07 +0800

    Add files via upload

---
+++ demo.m
@@ -0,0 +1,25 @@
+    load drugsim;
+    load virussim;
+    load virusdrug;
+    Vp=find(virusdrug()==1);
+    Vn=find(virusdrug()==0);
+    MatPredict=zeros(218,34);
+    Ip=crossvalind('Kfold',numel(Vp),5);
+    In=crossvalind('Kfold',numel(Vn),5);
+
+    for I=1:5
+        vp=Ip==I;
+        vn=In==I;
+        matDT=virusdrug;
+        matDT(Vp(vp))=0;
+        recMatrix=WRMF(B1,drugsim,virussim,30,5);
+        V=[Vn(vn);Vp(vp)];
+        MatPredict(V)=recMatrix(V);
+    end
+    [AUC,AUPR,Acc,Sen,Spe,Pre]=ROCcompute(MatPredict(),virusdrug(),1);
+
+        
+       
+       
+  
+
